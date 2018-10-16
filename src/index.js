610c0c513c8c127975a8af5b32e3a509e38d2161
Devesh <devsrawt@gmail.com>
2018-10-17 00:50:52 +0530

    First Commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import React,{ Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyBYaGB686fbrGL6phw9sGT5h8lNhDqvpns';
+
+class App extends Component{
+	constructor(props){
+		super(props);
+
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY ,term: 'surfbords'},(videos) => {
+			this.setState({ videos });
+		});
+	}
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList  videos={this.state.videos} />
+                
+            </div>
+        );        
+    }
+}
+
+
+ReactDOM.render( < App /> , document.querySelector('.container'));
